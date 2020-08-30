dd59d962a65790b79b2e70f918b495ec2fe72cab
Tipu <tipu3001@gmail.com>
2020-08-30 12:26:19 +0600

    ch11

--- books/tests.py
+++ books/tests.py
@@ -1,19 +1,32 @@
 # books/tests.py
-
+from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
 
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
 
     def setUp(self):
+
+        self.user = get_user_model().objects.create_user(
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
+
         self.book = Book.objects.create(
             title='Harry Potter',
             author='Jk Rowling',
             price='25.00',
+        )
+
+        self.review = Review.objects.create(
+            book=self.book,
+            author=self.user,
+            review='An excellent review',
         )
 
     def test_book_listing(self):
@@ -27,4 +40,13 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertTemplateUsed(response, 'books/book_detail.html')
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get('/books/12345')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')
         self.assertTemplateUsed(response, 'books/book_detail.html')
